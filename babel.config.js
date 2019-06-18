b51524c5df0123b0bfbffecf72a195b867f7e53b
Christoph Wurst <christoph@winzerhof-wurst.at>
2019-06-18 10:06:27 +0200

    Use shared browserslist config
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- babel.config.js
+++ babel.config.js
@@ -4,13 +4,6 @@ module.exports = {
         [
             "@babel/env",
             {
-                targets: {
-                    chrome: "67",
-                    edge: "17",
-                    firefox: "60",
-                    ie: "11",
-                    safari: "11.1",
-                },
                 useBuiltIns: "usage",
                 corejs: "3.0.0",
             },
