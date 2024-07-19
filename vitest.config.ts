da1f19a98a60e99750930db322b451f4e1c8f951
Ferdinand Thiessen <opensource@fthiessen.de>
2024-07-19 23:18:24 +0200

    fix: Correctly setup Typescript root to emit declarations
    Previously no root was set, so types for `vite.config` were included and
    the library types were emitted to `dist/lib`.
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,23 +2,24 @@
  * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
+import { defineConfig } from "vitest/config";
 import viteConfig from "./vite.config";
 
 export default async (env: Parameters<typeof viteConfig>[0]) => {
 	const config =
 		typeof viteConfig === "function" ? await viteConfig(env) : viteConfig;
 
-	// node-externals conflicts with vitest
-	config.plugins = config.plugins!.filter(
-		(plugin) =>
-			plugin && (!("name" in plugin) || plugin?.name !== "node-externals")
-	);
-
-	config.test = {
-		coverage: {
-			reporter: ["text", "lcov"],
-		},
-	};
-
-	return config;
-};
+	return defineConfig({
+		...config,
+		// node-externals conflicts with vitest
+		plugins: config.plugins!.filter(
+			(plugin) =>
+				plugin && (!("name" in plugin) || plugin?.name !== "node-externals")
+		),
+		test: {
+			coverage: {
+				reporter: ["text", "lcov"],
+			},
+		}
+	})
+}
